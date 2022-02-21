aa00d55b86b5288a2ba3f28ce5f7583341b1e830
Pink-Hat-Hacker <zyv@udel.edu>
2022-02-20 21:08:44 -0500

    2 funcs down a lot to go

--- src/arrays.ts
+++ src/arrays.ts
@@ -5,7 +5,11 @@
  * the number twice.
  */
 export function bookEndList(numbers: number[]): number[] {
-    return numbers;
+    if (numbers.length != 0) {
+        const num2 = [numbers[0], numbers[numbers.length - 1]];
+        return num2;
+    }
+    return [];
 }
 
 /**
@@ -13,7 +17,8 @@ export function bookEndList(numbers: number[]): number[] {
  * number has been tripled (multiplied by 3).
  */
 export function tripleNumbers(numbers: number[]): number[] {
-    return numbers;
+    const triple = numbers.map((num: number): number => num * 3);
+    return triple;
 }
 
 /**
@@ -21,6 +26,9 @@ export function tripleNumbers(numbers: number[]): number[] {
  * the number cannot be parsed as an integer, convert it to 0 instead.
  */
 export function stringsToIntegers(numbers: string[]): number[] {
+    {
+        /*const num2 = numbers.map((str: string): number => number);*/
+    }
     return [];
 }
 
