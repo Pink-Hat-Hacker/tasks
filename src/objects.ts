3309988f4c6c925e7bab62773909e57df8ca3dd7
Pink-Hat-Hacker <zyv@udel.edu>
2022-02-23 17:09:44 -0500

    2 done

--- src/objects.ts
+++ src/objects.ts
@@ -30,7 +30,12 @@ export function makeBlankQuestion(
  * HINT: Look up the `trim` and `toLowerCase` functions.
  */
 export function isCorrect(question: Question, answer: string): boolean {
-    return false;
+    const a_q = {
+        ...question,
+        expected: question.expected.trim().toLowerCase()
+    };
+    const a_a = answer.trim().toLowerCase();
+    return a_q.expected === a_a ? true : false;
 }
 
 /**
