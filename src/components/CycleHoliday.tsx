aa41f3f23c802b31751d3b10b203f1bda3c18d75
Pink-Hat-Hacker <zyv@udel.edu>
2022-03-06 14:37:58 -0500

    starting holiday -- trying to make a type

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
-
+//fav holidays: halloween, christmas, new year, chinese new year, thanksgiving
+//emojis: 🎄, 🦃, 🎆, 🧧, 🎃
 export function CycleHoliday(): JSX.Element {
+    type holiday = "Halloween", "Christmas", "New Year", "Chinese New Year", "Thanksgiving";
+    const [holi, setHoliday] = useState<holiday>("New Year");
     return <div>Cycle Holiday</div>;
 }
