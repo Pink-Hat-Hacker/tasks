14d3f03a74abfbad0c58cc41eda4a1fbb557937e
Pink-Hat-Hacker <zyv@udel.edu>
2022-03-12 15:26:43 -0500

    this is what i get for making weirdd names damnit

--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 /*
 The EditMode component has two states controlled by a switch: 
 the default state shows text indiciating the user’s name and whether 
@@ -16,14 +17,55 @@ As another example, if the user entered their name as Charles Babbage
 and said they were NOT a student, then the text would be Charles Babbage 
 is not a student.
 */
+type ChangeEvent = React.ChangeEvent<
+    HTMLTextAreaElement | HTMLInputElement | HTMLSelectElement
+>;
 
 export function EditMode(): JSX.Element {
-    const [userName, setUserName] = useState<string>("HELLO");
+    const [userName, setUserName] = useState<string>("Your Name");
     const [isStudent, setStudent] = useState<boolean>(true);
     const [editUserName, setEditUserName] = useState<boolean>(false);
     return (
         <div>
             <h3>Edit Mode</h3>
+            <Form.Check
+                type="switch"
+                id="editMode"
+                label="Edit?"
+                checked={editUserName}
+                onChange={() => setEditUserName(!editUserName)}
+            />
+            {editUserName && (
+                <Form.Check
+                    type="switch"
+                    id="a-student"
+                    label="Are you a student?"
+                    checked={isStudent}
+                    disabled={!editUserName}
+                    onChange={() => setStudent(!isStudent)}
+                />
+            )}
+            {editUserName && (
+                <Form.Group controlId="userName">
+                    <Form.Label>What is your name?</Form.Label>
+                    <Form.Control
+                        type="text"
+                        value={userName}
+                        disabled={!editUserName}
+                        onChange={(event: ChangeEvent) =>
+                            setUserName(event.target.value)
+                        }
+                    />
+                </Form.Group>
+            )}
+            <div>
+                {editUserName === false && isStudent === true && (
+                    <span> {userName} is a student. </span>
+                )}
+                {editUserName === false && isStudent === false && (
+                    <span>{userName} is not a student</span>
+                )}
+            </div>
         </div>
     );
 }
